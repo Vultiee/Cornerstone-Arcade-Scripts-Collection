dc642611ba94e01e0ec36b2ae314bbac57e90d32
Vultiee <80932924+Vultiee@users.noreply.github.com>
2022-11-23 19:37:59 -0500

    Create Jump Dome - Remove Yellow Arm.lua

---
+++ Jump Dome/Jump Dome - Remove Yellow Arm.lua
@@ -0,0 +1,3 @@
+game:GetService("Workspace").Games["Jump Dome Deluxe"].ArmSystem.Arm:Destroy() -- Destroys the yellow arm from the Jump Rope arcade game
+
+--Make sure to hold down your spacebar to get points in the game
