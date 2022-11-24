353905a5ef6e480f4a26ba6c752f0aa45796a894
Vultiee <80932924+Vultiee@users.noreply.github.com>
2022-11-23 19:38:25 -0500

    Add files via upload

--- Jump Dome/Jump Dome - Remove Yellow Arm.lua
+++ Jump Dome/Jump Dome - Remove Yellow Arm.lua
@@ -1,3 +1,5 @@
-game:GetService("Workspace").Games["Jump Dome Deluxe"].ArmSystem.Arm:Destroy() -- Destroys the yellow arm from the Jump Rope arcade game
-
---Make sure to hold down your spacebar to get points in the game
+game:GetService("Workspace").Games["Dig Down"].GoalSensor.Size = Vector3.new(50, 55, 1.5) -- Changes size to make you win once the ball is spawned
+game:GetService("Workspace").Games["Dig Down"].GoalSensor.Transparency = 0.25 -- Change transparency to make it slightly visible
+wait(3.5)
+game:GetService("Workspace").Games["Dig Down"].GoalSensor.Size = Vector3.new(0.2, 0.1, 1.6) -- Changes back to original size
+game:GetService("Workspace").Games["Dig Down"].GoalSensor.Transparency = 0 -- Changes back to original transparency
